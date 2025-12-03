1404d1a4cc8a345b3beb4f69a3bbbb90f15b9c38
YOUSSEF TAWK <YSTawk@gmail.com>
2025-12-03 03:31:05 +0000

    reorder

---
+++ deploy/07_STORES.sql
@@ -0,0 +1,15 @@
+-- This file was created by SleekData (https://www.youtube.com/@sleekdata)
+-- You may use it for personal, educational purposes only.
+-- Unauthorized commercial use of this file or its data, including use in other YouTube videos, is prohibited.
+
+INSERT INTO "L1_LANDING"."STORES" ("STOREID", "STORENAME", "ADDRESS", "CITY", "STATE", "ZIPCODE", "EMAIL", "PHONE", "UPDATED_AT") VALUES
+('1305', 'houston44', '3333 Villanueva Pike Apt. 005', 'Houston', 'TX', '77001', 'houston44@sleekmart.com', '939.693.8946x036', '2025-06-30 09:33:43'),
+('1794', 'chicago36', '330 Clark River Apt. 263', 'Chicago', 'IL', '60601', 'chicago36@sleekmart.com', '5873568473', '2025-06-21 05:24:28'),
+('1506', 'losangeles51', '871 Logan Mall Suite 950', 'Los Angeles', 'CA', '90001', 'losangeles51@sleekmart.com', '+1-906-437-9637x054', '2025-06-22 01:04:29'),
+('1321', 'newyork67', '260 Mcdaniel Trail', 'New York', 'NY', '10001', 'newyork67@sleekmart.com', '(211)441-4251', '2025-06-11 02:22:21'),
+('1462', 'houston68', '0491 Lyons Ferry', 'Houston', 'TX', '77001', 'houston68@sleekmart.com', '758-340-4580', '2025-06-23 11:49:58'),
+('1308', 'chicago72', '25025 Andersen Extension Suite 017', 'Chicago', 'IL', '60601', 'chicago72@sleekmart.com', '570.717.8424x736', '2025-06-04 10:32:53'),
+('1069', 'chicago40', '214 Diana Harbor', 'Chicago', 'IL', '60601', 'chicago40@sleekmart.com', '(621)734-5230x83953', '2025-06-13 09:50:28'),
+('1128', 'houston88', '198 Julie Mills Apt. 495', 'Houston', 'TX', '77001', 'houston88@sleekmart.com', '789.384.1450', '2025-06-08 17:24:59'),
+('1887', 'chicago95', '639 Paula Skyway Suite 302', 'Chicago', 'IL', '60601', 'chicago95@sleekmart.com', '493.978.7338x0236', '2025-06-27 05:44:44'),
+('1775', 'losangeles32', '8285 Sweeney Branch', 'Los Angeles', 'CA', '90001', 'losangeles32@sleekmart.com', '+1-240-568-9518', '2025-06-11 10:35:44');
